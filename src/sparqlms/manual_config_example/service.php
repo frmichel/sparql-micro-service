11156aedacef7d8c3eca0d6fdc72043a9f859278
Franck Michel <frmichel7@yahoo.fr>
2018-07-26 17:00:25 +0200

    Fix refactoring error on getQueryStringArgs

--- src/sparqlms/manual_config_example/service.php
+++ src/sparqlms/manual_config_example/service.php
@@ -11,7 +11,7 @@ $logger = $context->getLogger();
 
 // Get the service custom arguments defined in config.ini: param1 and param2
 $customArgs = $context->getConfigParam('custom_parameter');
-list ($param1, $param2) = array_values($context->getQueryStringArgs($customArgs));
+list ($param1, $param2) = array_values(getQueryStringArgs($customArgs));
 
 /*
  * ----------------- Your specific code --------------------
