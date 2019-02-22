ddac851df00fc49d40bb7578cd018542a2cd9e27
Franck Michel <frmichel7@yahoo.fr>
2019-02-22 11:09:51 +0100

    Add customization of EoL token in deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-# This script customizes the configuration files (config.ini) and SPARQL file (insert.sparql, construct.sparql)
-# of each SPARQL micro-serive by replacing the "example.org" hostname and "<api_key>" values commited on the public repo.
+# This script customizes the configuration files (config.ini), SPARQL files (insert.sparql, construct.sparql)
+# and service description files (ServiceDescription.ttl) of each SPARQL micro-serive by replacing 
+# the "example.org" hostname and "<api_key>" values commited on the public repo.
 #
+# Usage: 
 # Copy this script in the public_html folder where the SPARQL micro-service code is installed
 # (there must be an src subfolder here). CD to public_html and run the script.
 
-# The machine where the services are deployed. Will replace the 'example.org'
+# The machine where the services are deployed. Will replace the 'http://example.org'
 SERVER='http:\/\/sms.i3s.unice.fr'
 
 # Directory where to search for SPARQL microservices, from the directory where this script is launched
 SMSDIR=src/sparqlms
 
 function substitute() {
-    # Reset commited version
+    # Optional: first reset commited version
     #git checkout HEAD -- $3
-
     sed "s/$1/$2/g" $3 > $3.tmp
     mv $3.tmp $3
 }
@@ -39,6 +40,15 @@ for FILE in $(ls $SMSDIR/flickr/*/ServiceDescription*.ttl 2> /dev/null); do
     echo "Changing $replace into $API_KEY in $FILE"
     substitute "$replace" "$API_KEY" "$FILE"
 done
+
+# --- EoL API token ---
+API_KEY= <paste your api token here>
+for FILE in $(ls $SMSDIR/eol/*/config.ini 2> /dev/null); do
+    replace='<api_personal_token>'
+    echo "Changing $replace into $API_KEY in $FILE"
+    substitute "$replace" "$API_KEY" "$FILE"
+done
+
 
 # --- Replace example.org with local server URL in sparql files ---
 for FILE in `ls $SMSDIR/*/*/*.sparql`; do
