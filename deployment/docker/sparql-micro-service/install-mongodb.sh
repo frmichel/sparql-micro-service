cd6a7c25a25e8b98eb9c843eb2f8b08b4a9a0653
Franck Michel <frmichel7@yahoo.fr>
2020-08-17 10:40:05 +0200

    Merge v050_dev

--- deployment/docker/sparql-micro-service/install-mongodb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/mongodb/mongo-php-driver.git
-cd mongo-php-driver
-
-pecl install mongodb
-echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
